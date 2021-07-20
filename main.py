9b8fbceae71f0203508d384bf17d4c349868d77a
Gustavo Fonseca <44001877+fonsecagustavo@users.noreply.github.com>
2021-07-20 18:32:40 -0300

    Mais funções

--- main.py
+++ main.py
@@ -1,8 +1,67 @@
 import numpy as np
 
-arquivo = input("Digite o nome do arquivo")
-ref_arquivo = open(arquivo + '.txt','r')
+def menu():
+    arquivo = input("Digite o nome do arquivo")
+    ref_arquivo = open(arquivo + '.txt', 'r')
+    arquivo = SplitList(ref_arquivo)
 
+    representa = int(input("Digite 1 para Lista, 2 Para Matriz ou 0 para sair."))
+    while representa != 0:
+        if representa == 1:
+            menuLista(arquivo)
+            representa = int(input("Digite 1 para Lista, 2 Para Matriz ou 0 para sair."))
+        elif representa == 2:
+            menuMatriz(arquivo)
+            representa = int(input("Digite 1 para Lista, 2 Para Matriz ou 0 para sair."))
+        else:
+            representa = int(input("Digite 1 para Lista, 2 Para Matriz ou 0 para sair."))
+
+def menuLista(arquivo):
+    lista = ConvertList(arquivo)
+    escolha = 1
+    while escolha != 0:
+        print("1 - Representação")
+        print("2 - Informações")
+        print("3 - Busca em Grafos: Largura")
+        print("4 - Busca em Grafos:  Profundidade")
+        print("5 -Componentes Conexos")
+        print("0 - Voltar")
+        escolha = int(input(""))
+        if escolha == 1:
+            for lis in lista:
+                print(lis)
+        elif escolha == 2:
+            GrauList(lista)
+        elif escolha == 3:
+            BuscaLarguraLista(lista,0)
+        elif escolha == 4:
+            BuscaProfundidadeLista(lista,0)
+        elif escolha == 5:
+            print("Sem")
+
+def menuMatriz(arquivo):
+    matriz = ConvertMatriz(arquivo)
+    escolha = 1
+    while escolha != 0:
+        print("1 - Representação")
+        print("2 - Informações")
+        print("3 - Busca em Grafos: Largura")
+        print("4 - Busca em Grafos:  Profundidade")
+        print("5 -Componentes Conexos")
+        print("0 - Voltar")
+        escolha = int(input(""))
+        if escolha == 1:
+            print(matriz)
+        elif escolha == 2:
+            GrauMatriz(matriz)
+        elif escolha == 3:
+            BuscaLarguraMatriz(matriz,0)
+        elif escolha == 4:
+            BuscaProfundidadeMatriz(matriz,0)
+        elif escolha == 5:
+            print("Sem")
+
+#transforma os dados em uma lista
 def SplitList(arq):
     teste = []
     for linha in arq:
@@ -10,6 +69,7 @@ def SplitList(arq):
         teste.append(valores)
     return teste
 
+#Converte os dados em uma matriz de adjacentes
 def ConvertMatriz(lists):
     index = True
     for list in lists:
@@ -23,6 +83,7 @@ def ConvertMatriz(lists):
             matriz[j,i] = int(list[2])
     return matriz
 
+#Converte os dados em uma lista de adjacentes
 def ConvertList(lists):
     index = True
     for list in lists:
@@ -37,6 +98,28 @@ def ConvertList(lists):
             list_adj[j].append([i, x])
     return list_adj
 
+#Começa separando quantas conexões cada um tem
+#Um para a lista e outro para matriz
+def GrauList(lists):
+    grau = []
+    for list in lists:
+        grau.append(len(list))
+    Grau(grau)
+
+def GrauMatriz(matriz):
+    n = len(matriz)
+    grau = [[] for _ in range(n)]
+    index = 0
+    for mat in matriz:
+        g = 0
+        for m in mat:
+            if(m > 0):
+                g += 1
+        grau[index] = g
+        index += 1
+    Grau(grau)
+
+#chaga os dados e encontra o maior e o menor grau
 def Grau(grau):
     max = grau[0]
     min = grau[0]
@@ -55,6 +138,7 @@ def Grau(grau):
     print("Menor grau: ", min, " Vertice: ", verticeMin)
     GrauMedio(grau,max)
 
+#Econtra o grau medio
 def GrauMedio(grau,max):
     grauMedio = []
     totalGrauMedio = 0
@@ -70,6 +154,7 @@ def GrauMedio(grau,max):
     print("Grau medio: ",grauMedioTotal)
     FrequenciaGrau(grau,grauMedio,max)
 
+#E a frequencia de cada grau
 def FrequenciaGrau(grau,grauMedio,max):
     x = len(grau)
     frequencia = [0 for _ in range(max+1)]
@@ -90,25 +175,7 @@ def FrequenciaGrau(grau,grauMedio,max):
             print("Grau ", index,": ", freq)
         index += 1
 
-def GrauList(lists):
-    grau = []
-    for list in lists:
-        grau.append(len(list))
-    Grau(grau)
-
-def GrauMatriz(matriz):
-    n = len(matriz)
-    grau = [[] for _ in range(n)]
-    index = 0
-    for mat in matriz:
-        g = 0
-        for m in mat:
-            if(m > 0):
-                g += 1
-        grau[index] = g
-        index += 1
-    Grau(grau)
-
+#Busca em largura para lista de adjacentes
 def BuscaLarguraLista(G, s):
     desc = [0 for _ in range(len(G))]
     nivel = [-1 for _ in range(len(G))]
@@ -118,7 +185,6 @@ def BuscaLarguraLista(G, s):
     nivel[s] = 0
     while len(Q) != 0:
         u = Q.pop(0)
-        print("teste",u)
         n = nivel[u]+1
         for v in G[u]:
             if desc[v[0]] == 0:
@@ -132,6 +198,7 @@ def BuscaLarguraLista(G, s):
         if nivel[i] >= 0:
             print(i, ": ", nivel[i])
 
+#Busca em largura para matriz de adjacentes
 def BuscaLarguraMatriz(G, s):
     desc = [0 for _ in range(len(G))]
     nivel = [-1 for _ in range(len(G))]
@@ -157,25 +224,63 @@ def BuscaLarguraMatriz(G, s):
         if nivel[i] >= 0:
             print(i, ": ", nivel[i])
 
-def BuscaProfundidade (G,s):
-    desc = [0 for i in range(len(G))]
+#Busca em profundidade para lista de adjacentes
+def BuscaProfundidadeLista (G,s):
+    desc = [0 for _ in range(len(G))]
+    nivel = [-1 for _ in range(len(G))]
     S = [s]
     R = [s]
     desc[s] = 1
+    nivel[s] = 0
     while len(S) != 0:
         u = S[-1]
         desempilhar = True
+        n = nivel[u] + 1
         for v in G[u]:
-            if desc[v] == 0:
+            if nivel[v[0]] == -1 or nivel[v[0]] >= n:
+                nivel[v[0]] = n
+            if desc[v[0]] == 0:
                 desempilhar = False
-                S.append(v)
-                R.append(v)
-                desc[v] = 1
+                S.append(v[0])
+                R.append(v[0])
+                desc[v[0]] = 1
                 break
         if desempilhar:
             S.pop()
+    print("Vertice:Nivel")
+    for i in range(len(desc)):
+        if nivel[i] >= 0:
+            print(i, ": ", nivel[i])
 
+#Busca em profundidade para matriz de adjacentes
+def BuscaProfundidadeMatriz (G,s):
+    desc = [0 for _ in range(len(G))]
+    nivel = [-1 for _ in range(len(G))]
+    S = [s]
+    R = [s]
+    desc[s] = 1
+    nivel[s] = 0
+    while len(S) != 0:
+        u = S[-1]
+        desempilhar = True
+        n = nivel[u] + 1
+        index = 0
+        for v in G[u]:
+            if v > 0:
+                if nivel[index] == -1 or nivel[index] >= n:
+                    nivel[index] = n
+                if desc[index] == 0:
+                    desempilhar = False
+                    S.append(index)
+                    R.append(index)
+                    desc[index] = 1
+                    break
+            index += 1
+        if desempilhar:
+            S.pop()
+    print("Vertice:Nivel")
+    for i in range(len(desc)):
+        if nivel[i] >= 0:
+            print(i, ": ", nivel[i])
 
-tes = SplitList(ref_arquivo)
-te = ConvertMatriz(tes)
-BuscaLarguraMatriz(te,0)
+menu()
